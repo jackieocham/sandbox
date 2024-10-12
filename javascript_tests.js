e05e4304458108927ebc62bf0114140e9595b2a4
Jackie <jackieo.cham@gmail.com>
2024-10-12 15:15:22 -0400

    random husband tutorial

---
+++ javascript_tests.js
@@ -0,0 +1,12 @@
+// function nameFunction(arguments/parameters) {
+// describe what function will do with arguments
+// using program language - javascript
+// };
+
+function testFunction(input1, input2) {
+    console.log("input 1: " + input1);
+    console.log("input 2: " + input2);
+    console.log("total: " + (input1 + input2));
+};
+testFunction(1, 2);
+testFunction("happy", "sad");
